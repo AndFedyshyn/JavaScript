8c7a05e77110d40ea156846694492ff71ad6fd1a
AndFedyshyn <andfedyshyn@gmail.com>
2020-10-29 12:13:57 +0200

    'home_work_1_js'

---
+++ JSHW1/hw1.js
@@ -0,0 +1,266 @@
+// Завдання 1
+
+let hello = ('hello');
+let owu = ('owu');
+let com = ('com');
+let ua = ('ua');
+let one = (1);
+let ten = (10);
+let minus999 = (-999);
+let num123 = (123);
+let pi = (3.14);
+let twoSeven = (2.7);
+let sixteen = (16);
+let istrue=(true);
+let isfalse=(false);
+
+alert(hello);
+alert(owu);
+alert(com)
+alert(ua);
+alert(one);
+alert(ten);
+alert(minus999);
+alert(num123);
+alert(pi);
+alert(twoSeven);
+alert(sixteen);
+alert(istrue);
+alert(isfalse);
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(minus999);
+console.log(num123);
+console.log(pi);
+console.log(twoSeven);
+console.log(sixteen);
+console.log(istrue);
+console.log(isfalse);
+
+document.write(hello);
+document.write(owu);
+document.write(com);
+document.write(ua);
+document.write(one);
+document.write(ten);
+document.write(minus999);
+document.write(num123);
+document.write(pi);
+document.write(twoSeven);
+document.write(sixteen);
+document.write(istrue);
+document.write(isfalse);
+
+// Завдання 2//
+
+hello = 'hi' ;
+owu = 'school';
+com = 'uaaa';
+ua = 'super';
+one = '111';
+ten ='1000';
+minus999 = -8888;
+num123 = '456';
+pi = 'piiiiiiiiiiiiiiii';
+twoSeven = '222227777777';
+sixteen = '564686465';
+istrue = false;
+isfalse= true;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(minus999);
+console.log(num123);
+console.log(pi);
+console.log(twoSeven);
+console.log(sixteen);
+console.log(istrue);
+console.log(isfalse);
+
+document.write(hello);
+document.write(owu);
+document.write(com);
+document.write(ua);
+document.write(one);
+document.write(ten);
+document.write(minus999);
+document.write(num123);
+document.write(pi);
+document.write(twoSeven);
+document.write(sixteen);
+document.write(istrue);
+document.write(isfalse);
+
+// Завдання 3 ////
+
+const dollar = (30);
+const euro = (500);
+const uah = (10000);
+const country = (USA);
+const city = (Paris);
+const sea = (black);
+
+
+console.log(dollar);
+console.log(euro);
+console.log(uah);
+console.log(country);
+console.log(city);
+console.log(sea);
+
+document.write(dollar);
+document.write(euro);
+document.write(uah);
+document.write(country);
+document.write(city);
+document.write(sea);
+
+alert(dollar);
+alert(euro);
+alert(uah);
+alert(country);
+alert(city);
+alert(sea);
+
+// Завдання 4 ///
+
+let myname = prompt('Enter your Name');
+let Surname = prompt('Enter your Surname');
+let fname = prompt('Fathers name');
+
+console.log(myname);
+console.log(Surname);
+console.log(fname);
+
+alert(myname);
+alert(Surname);
+alert(fname);
+
+document.write(myname);
+document.write(Surname);
+document.write(fname);
+
+// Завдання 5///
+
+let person = myname + ' ' + Surname + ' ' + fname;
+console.log(person);
+
+// Завдання 6
+
+let fathername = prompt('Enter your fathers Name');
+let fathersurname = prompt('Enter your fathers Surname');
+let ffname = prompt('Enter your fathers Name');
+let mothername = prompt('Enter your mothers name');
+let mothersurname = prompt('Enter your mothers Surname');
+let mfname = prompt('Enter your mothers fathers name');
+
+let father = fathername + ' ' + fathersurname + ' ' + ffname;
+console.log(father);
+let mother = mothername + ' ' + mothersurname + ' ' + mfname;
+console.log(mother);
+
+Завдання 7 ///
+
+let num1 = +prompt('Enter first number');
+let num2 = +prompt('Enter second number');
+let num3 = +prompt('Enter third number');
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+Завдання 8////
+
+let num888 = prompt('Enter first number');
+let num999 = prompt('Enter second number');
+let num555 = prompt('Enter third number');
+let num333 = prompt('Enter fourth number');
+
+let result =
+  parseInt(num888) +
+  parseInt(num999) +
+  parseInt(num555) +
+  parseInt(num333);
+console.log(result);
+
+Завдання 9//
+
+let num228 = prompt('Ввести бла - бла');
+let num229 = prompt('Ввести бла - бла');
+let num445 = prompt('Ввести бла - бла');
+
+console.log(parseFloat(num228));
+let resultt =
+  parseFloat(num228) + parseFloat(num229) + parseFloat(num445);
+console.log(resultt);
+
+Завдання 10 //
+
+let result1 =
+  Math.round(num228) + Math.round(num229) + Math.round(num445);
+console.log(result1);
+
+Завдання 11//
+
+let num8 = +prompt('Ввести 8');
+let num9 = +prompt('Ввести 9');
+
+console.log(Math.pow(num8, num9));
+
+Завдання 12 //
+
+let aaa = 100;
+let bbb = '100';
+let ccc = true;
+let ddd = undefined;
+
+console.log(typeof aaa);
+console.log(typeof bbb);
+console.log(typeof ccc);
+console.log(typeof ddd);
+
+Завдання 13//
+
+let j = 5 < 6;
+console.log(j);
+
+let k = 5 > 6;
+console.log(k);
+
+let m = 5 >= 6;
+console.log(m);
+
+let sss = 5 == 6;
+console.log(sss);
+
+let xxx = 10 == 10;
+console.log(xxx);
+
+let yyy = 10 === 10;
+console.log(yyy);
+
+let nnn = 10 > 10;
+console.log(nnn);
+
+let qqq = 10 < 10;
+console.log(qqq);
+
+let iii = 10 != 10;
+console.log(iii);
+
+let uuu = 123 === '123';
+console.log(uuu);
+
+let ttt = 123 == '123';
+console.log(ttt);
+
+
