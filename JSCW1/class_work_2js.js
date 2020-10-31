babbfc7bc06d0e67c51a5f36b83b3eecce83a33d
AndFedyshyn <andfedyshyn@gmail.com>
2020-10-31 22:24:00 +0200

    class_work_1

---
+++ JSCW1/class_work_2js.js
@@ -0,0 +1,65 @@
+// 1.Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true».
+// При помощи оператора typeof убедитесь, что значения переменных принадлежат типам: string, number и  boolean.
+
+
+const str = "Привіт";
+const num = 123;
+const flag = true;
+const txt = "true";
+
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(flag));
+console.log(typeof(txt));
+
+// 2. Создайте переменные a1, a2, a3, a4, a5. При помощи математических операторов (сложение, вычитание и т.д.) найдите значения выражений:
+//     5 + 3,
+//     5 - 3,
+//     5 * 3,
+//     5 / 3,
+//     5 % 3
+// поместив результат каждого выражения в соответствующую переменную. Например, let a1 = 5 + 3.
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+// let a1;
+// let a2;
+// let a3;
+// let a4;
+
+//     a1 = 5 + 3;
+//     a2 = 5 - 3;
+//     a3 = 5 * 3;
+//     a4 = 5 / 3;
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+
+// 3.  Создайте переменные a6, a7, a8, a9, a10. Поместите в них результат выражений:
+//     5 % 3,
+//     3 % 5,
+//     5 + '3',
+//     '5' - 3,
+//     75 + 'кг'
+
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
