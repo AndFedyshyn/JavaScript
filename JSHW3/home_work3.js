a158142307d349f715db8b4e7e8f3121ed90184f
AndFedyshyn <andfedyshyn@gmail.com>
2020-11-02 14:21:03 +0200

    home_work3

---
+++ JSHW3/home_work3.js
@@ -0,0 +1,69 @@
+console.log(2020)
+
+// 1.-- створити об'єкт (не меньше 5ти властивостей) який описує
+// // - собаку
+// // - людину
+// // - автомобіль
+// // - квартиру
+// // - книгу
+
+const dog ={
+    dog_breed : 'German Shepherd',
+    country : 'Germany',
+    lifeSpan : 13,
+    coat : 'double coat',
+    color : 'Tan with black saddle, sable, solid black or bi-color',
+    weight : 40,
+    height : 60,
+}
+console.log(dog)
+
+const human = {
+    age : 30,
+    height : 178,
+    weight : 81,
+    citizenship : 'Ukraine',
+    hobby : ['sport','football','mountain tourism'],
+    music : ['rock','new metal','instrumental'],
+}
+console.log(human)
+
+const car = {
+    brand : 'BMW',
+    model : 'X6',
+    year : 2020,
+    color : 'black',
+    kilometres : 1000,
+    status : 'used',
+    highlights : ['navigation System','leather Interior','excellent condition'],
+}
+console.log(car)
+
+const apartment = {
+
+    price : 50000,
+    year : 2019,
+    rooms : 3,
+    area : 80,
+    status: 'new',
+    city : 'Lviv',
+}
+console.log(apartment)
+
+const book = {
+    name : 'Справа Василя Стуса. Збірка документів з архіву КДБ УРСР',
+    written : 'Vahtang Kipiani',
+    publisher: 'Vivat',
+    year : 2020,
+    language: 'Ukrainian',
+    pages : 688,
+    illustrations : 'Yes',
+    format : '147x205x46'
+}
+console.log(book)
+
+// 2.-- Створити масив та вивести його в консоль:
+//     - з 5 собак
+// - 3 5 людей
+// - з 5 автомобілів
+
