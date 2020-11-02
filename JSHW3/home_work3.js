785d9ce01e4344149c6f31d08f852a4b917da4b9
AndFedyshyn <andfedyshyn@gmail.com>
2020-11-02 16:36:12 +0200

    home_work3

--- JSHW3/home_work3.js
+++ JSHW3/home_work3.js
@@ -63,7 +63,74 @@ const book = {
 console.log(book)
 
 // 2.-- Створити масив та вивести його в консоль:
-//     - з 5 собак
+// - з 5 собак
 // - 3 5 людей
 // - з 5 автомобілів
+
+const arrDog = ['German Shepherd', 'collie', 'bobermann', 'stbernard', 'labrador ']
+const arrPeople = ['Marko', 'Andrii', 'Tania', 'Viktoria', 'Veronika']
+const arrCars = ['BMW', 'Mercedes', 'Audi', 'Jeep', 'Nisan']
+console.log(arrDog);
+console.log(arrPeople);
+console.log(arrCars);
+
+
+// 3.- створити об'єкт (не меньше 5ти властивостей) який описує, одна з властивостей обов'язково повинна бути об'єктом,ще одна - масивом
+// - будинок
+// - водій
+// - іграшку
+// - стіл
+// - сумка
+
+let apartment1;
+apartment1 = {
+
+    price: 50000,
+    year: 2019,
+    rooms: {num1: 1, num2: 2, num: 3},
+    area: 80,
+    status: 'new',
+    city: ['Lviv', 'old town', 'Pl. Rynok'],
+};
+console.log(apartment1);
+
+const driver = {
+    age : 30,
+    height : 178,
+    weight : 81,
+    citizenship : 'Ukraine',
+    hobby : {sport: 'football',fc:'Manchester United' },
+    music : ['rock','new metal','instrumental'],
+};
+console.log(driver)
+
+const toy = {
+    colors : {color1:'red',color2:'blue',color3:'green'},
+    width : 100,
+    weight : '150g',
+    madeIn : {country:'Ukraine',city: 'Kyiv'},
+    price : ['100 dol','2800 uah','85euro'],
+}
+console.log(toy);
+
+const table = {
+
+    price: '350 dol',
+    year: 2017,
+    materials: {material1:'tree',material2:'plastic',material3:'metal',} ,
+    status: ['New','using'],
+    mass: 40,
+};
+console.log(table);
+
+const bag = {
+
+    price: [100,240,300,399,500],
+    year: [2019,2020],
+    materials: {material:'polyester',cotton: {fiber1:'10mm',fiber2:'13mm',fiber3:'55mm'}},
+    status: ['New','using'],
+    mass: [300,500,2000],
+
+};
+console.log(bag);
 
