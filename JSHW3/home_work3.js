beb3d2610b401a99619a319fcfe3b7ffcb0a17b3
AndFedyshyn <andfedyshyn@gmail.com>
2020-11-02 16:50:11 +0200

    home_work3

--- JSHW3/home_work3.js
+++ JSHW3/home_work3.js
@@ -134,3 +134,49 @@ const bag = {
 };
 console.log(bag);
 
+//4. Дан массив:
+    let users = [
+        {name: 'vasya', age: 31, status: false},
+        {name: 'petya', age: 30, status: true},
+        {name: 'kolya', age: 29, status: true},
+        {name: 'olya', age: 28, status: false},
+        {name: 'max', age: 30, status: true},
+        {name: 'anya', age: 31, status: false},
+        {name: 'oleg', age: 28, status: false},
+        {name: 'andrey', age: 29, status: true},
+        {name: 'masha', age: 30, status: true},
+        {name: 'olya', age: 31, status: false},
+        {name: 'max', age: 31, status: true},
+    ];
+// - звернутися в відповідну ячейку масиву і відповідний параметр об'єкта і вивести в консольх наступне
+// - статус Андрія
+// - статус Максима
+// - ім'я передостаннього об'єкту
+// - ім'я третього об'єкта
+// - вік Олега
+// - вік Олі
+// - вік + ім'я 5го об'єкта
+// - вік + сатус Анни
+// Приклад: вивести ім'я 1го об'єкта. Відповідь: console.log (users [0] .name). Будьте уважні! 4й об'єкт має індес 3!
+
+console.log(users[7].status);
+console.log(users[4].status);
+console.log(users[users.length - 2].name);
+console.log(users[2].name);
+console.log(users[6].age);
+console.log(users[3].age);
+console.log(users[4].age +' '+ users[4].name);
+console.log(users[5].age + ' ' + users[5].status);
+
+// 5.-Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому.
+//
+// -- Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// - отримує текст з параграфа з id "content"
+// - отримує текст з блоку з id "rules"
+// - замініть текст параграфа з id 'content' на будь-який інший
+// - замініть текст параграфа з id 'rules' на будь-який інший
+// - змініть кожному елементу колір фону на червоний
+// - змініть кожному елементу колір тексту на синій
+// - отримати весь список класів елемента з id=rules і вивести їх в console.log
+// - отримати всі елементи з класом fc_rules
+// - поміняти колір тексту у всіх елементів fc_rules на червоний
