a324d6b00c59ba30ffd11163986b82240c85c236
AndFedyshyn <andfedyshyn@gmail.com>
2020-11-03 19:11:07 +0200

    home_work4_(functions)

--- JSHW4/home_work4.js
+++ JSHW4/home_work4.js
@@ -11,12 +11,12 @@ conArr([878, false, 2020, true, 'function',2018,'Lviv','Kyiv','Odesa',85512 ]);
 
 function random(x) {
     let a = [];
-    for (i = 0; i < x; i++) {
+    for (let i = 0; i < x; i++) {
         a[i] = Math.floor(Math.random() * x);
     }
     return conArr(a);
 }
-contArr = random(20);
+conArr = random(20);
 
 // 3.- створити функцію яка приймає три числа та виводить та повертає найменьше.
 
@@ -25,3 +25,41 @@ function getMin(x, y, z) {
 
 }
 console.log(getMin(222, 777, 50));
+
+// 4.- створити функцію яка приймає три числа та виводить та повертає найбільше.
+
+const getMax = (x2, y2, z2) => Math.max(x2, y2, z2);
+
+console.log(getMax(199, 1999, 19));
+
+// 5.- створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше.
+
+function returnMinMax(numbersArray) {
+    let minValue = numbersArray[0];
+    let maxValue = numbersArray[0];
+
+    for(let i=0; i<numbersArray.length; i++) {
+        if(numbersArray[i] > maxValue) {
+            maxValue = numbersArray[i];
+        }
+
+        if(numbersArray[i] < minValue) {
+            minValue = numbersArray[i];
+        }
+    }
+
+    console.log(maxValue);
+    return minValue;
+}
+
+console.log(returnMinMax([1, 5, 23, 6, 7, 43, 3, 6, 7, 234]));
+
+// 6.- створити функцію яка виводить масив.
+
+function arrayConsole(array) {
+    console.log(array);
+}
+
+arrayConsole([2020, 2021,2022,2023,2024]);
+
+// 7.- створити функцію яка повертає найбільше число з масиву.
